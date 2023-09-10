6b67894a49aacac3cde7c8a87db5c4f360332310
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2023-09-10 16:11:05 +0100

    https

--- spotbook/src/constants/index.js
+++ spotbook/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://ec2-34-245-164-31.eu-west-1.compute.amazonaws.com:8000/";
+export const API_URL = "https://ec2-34-245-164-31.eu-west-1.compute.amazonaws.com:8000/";
 export const ACCOUNTS_API_URL = API_URL + "accounts/api/";
 export const CLIPS_API_URL = API_URL + "clips/api/";
 export const SPOTS_API_URL = API_URL + "spots/api/";
