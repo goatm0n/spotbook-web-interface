7e72f0085501a8e7d8bb7a5758e1e499cffbc30f
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2023-09-10 15:40:18 +0100

    index

--- spotbook/src/constants/index.js
+++ spotbook/src/constants/index.js
@@ -4,4 +4,4 @@ export const CLIPS_API_URL = API_URL + "clips/api/";
 export const SPOTS_API_URL = API_URL + "spots/api/";
 export const PROFILES_API_URL = API_URL + "profiles/api/";
 export const DEFAULT_PROFILE_PIC = "https://i.stack.imgur.com/l60Hf.png";
-export const ROOT_URL = "ec2-18-203-68-51.eu-west-1.compute.amazonaws.com:8000/";
+export const ROOT_URL = "https://spotbook.pages.dev/map";
