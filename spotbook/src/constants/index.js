a66aa04ee78624ac28ed21e2b6073db2596b4f3e
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2023-09-10 01:57:28 +0100

    root_url

--- spotbook/src/constants/index.js
+++ spotbook/src/constants/index.js
@@ -1,7 +1,7 @@
-export const API_URL = "http://54.229.135.8:8000/";
+export const API_URL = "http://ec2-34-245-164-31.eu-west-1.compute.amazonaws.com:8000/";
 export const ACCOUNTS_API_URL = API_URL + "accounts/api/";
 export const CLIPS_API_URL = API_URL + "clips/api/";
 export const SPOTS_API_URL = API_URL + "spots/api/";
 export const PROFILES_API_URL = API_URL + "profiles/api/";
 export const DEFAULT_PROFILE_PIC = "https://i.stack.imgur.com/l60Hf.png";
-export const ROOT_URL = "http://spotbook-web-interface.s3-website-eu-west-1.amazonaws.com/";
+export const ROOT_URL = "ec2-18-203-68-51.eu-west-1.compute.amazonaws.com:8000/";
